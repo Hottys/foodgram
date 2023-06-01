1c20cd43b617d59f68d3c848a09985e1f2040baa
Misha Kartavcev <46868875+Hottys@users.noreply.github.com>
2023-06-01 21:45:29 +0700

    p

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -74,12 +74,23 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.environ.get('POSTGRES_DB', BASE_DIR / 'db.sqlite3'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'ENGINE': os.getenv(
+            'DB_ENGINE', default='django.db.backends.postgresql'
+        ), 
+        'NAME': os.getenv(
+            'DB_NAME', default='postgres'),
+        'USER': os.getenv(
+            'POSTGRES_USER', default='postgres'
+        ),
+        'PASSWORD': os.getenv(
+            'POSTGRES_PASSWORD', default='postgres'
+        ),
+        'HOST': os.getenv(
+            'DB_HOST', default='db'
+        ),
+        'PORT': os.getenv(
+            'DB_PORT', default='5432'
+        )
     }
 }
 
